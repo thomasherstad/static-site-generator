5c5461dfb346a508a79ba7ec09eea8752fb2a2d7
thomasherstad <github@theormsats.fea.st>
2024-07-02 10:32:18 +0200

    finished split nodes images

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -87,10 +87,33 @@ class TextInlineMarkdown(unittest.TestCase):
         text_extracted = extract_markdown_links(text)
         self.assertEqual(text_extracted, expected)
 
+    
+
+#Need to write tests for split_nodes_image() and split_nodes_link()
+    '''
     def test_split_nodes_image(self):
-        input_text = "not"
-        text_split = "done"
-        self.assertEqual()
+            input_text = "not"
+            text_split = "done"
+            text_expected = ""
+            self.assertEqual()
+    '''
+            
+    def test_text_to_textnodes(text):
+        input_text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+        expected = [
+                    TextNode("This is ", text_type_text),
+                    TextNode("text", text_type_bold),
+                    TextNode(" with an ", text_type_text),
+                    TextNode("italic", text_type_italic),
+                    TextNode(" word and a ", text_type_text),
+                    TextNode("code block", text_type_code),
+                    TextNode(" and an ", text_type_text),
+                    TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                    TextNode(" and a ", text_type_text),
+                    TextNode("link", text_type_link, "https://boot.dev"),
+                ]
+        
+
 
 if __name__ == "__main__":
     unittest.main()
